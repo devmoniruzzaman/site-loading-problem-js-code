ef960bf71f17aac76f31e23781db296d3e30ac03
Moniruzzaman <144223325+devmoniruzzaman@users.noreply.github.com>
2024-04-25 12:11:08 +0600

    loading-problem.js

--- README.md
+++
@@ -1,2 +0,0 @@
-# site-loading-problem-js-code
-site loading problem js code
---
+++ loading-problem.js
@@ -0,0 +1,8 @@
+# site-loading-problem-js-code
+site loading problem js code
+<!-- End Meta Pixel Code -->
+<script type="text/javascript">
+var elm=document.getElementsByTagName("html")[0];
+elm.style.display="none";
+document.addEventListener("DOMContentLoaded",function(event) {elm.style.display="block"; });
+</script>
